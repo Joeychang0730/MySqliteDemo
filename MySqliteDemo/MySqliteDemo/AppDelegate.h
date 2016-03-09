8a131de3de628d3d529622216f07c74d06035860
changyou <changyou0730@gmail.com>
2016-03-09 23:13:54 +0800

    initial project

---
+++ MySqliteDemo/MySqliteDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySqliteDemo
+//
+//  Created by chang on 15/12/5.
+//  Copyright © 2015年 chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MySqliteDemo/MySqliteDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySqliteDemo
+//
+//  Created by chang on 15/12/5.
+//  Copyright © 2015年 chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
